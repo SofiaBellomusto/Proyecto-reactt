7519fcd2f3d807793354efeebce961a72485ab08
SofiaBellomusto <141052990+SofiaBellomusto@users.noreply.github.com>
2024-02-18 23:30:13 -0300

    ItemListContainer update

--- src/components/pages/ItemListContainer/ItemListContainer.jsx
+++ src/components/pages/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,30 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
 import ProductCard from "../../common/productCard";
+import "./ItemListContainer.css";
 
 const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("../../../../data/ItemList.json")
+      .then((response) => response.json())
+      .then((products) => setProducts(products));
+  }, []);
+
   return (
-    <div>
-      <h2>Aca van los productos</h2>
-      <ProductCard titulo="Bali" desc="las mejores" precio="$1200" />
-      <ProductCard titulo="Chufi" desc="las mejores" precio="$1200" />
-      <ProductCard titulo="ElfWorld" desc="las mejores" precio="$1400" />
+    <div className="container">
+      {products.map((product) => (
+        <ProductCard
+          key={product.id}
+          className="product-card"
+          title={product.title}
+          desc={product.desc}
+          price={product.price}
+          img={product.imageName}
+          extension={product.extension}
+        />
+      ))}
     </div>
   );
 };
