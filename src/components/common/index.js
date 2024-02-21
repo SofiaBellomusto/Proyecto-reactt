3956ba972a3c6df1c609ea98c89f6ee16c326f75
SofiaBellomusto <sofiabellomusto2001@gmail.com>
2024-02-21 12:06:27 -0300

    minimal changes

---
+++ src/components/common/index.js
@@ -0,0 +1,4 @@
+export { CartWidget } from './CartWidget';
+export { default as CategoryList } from './CategoryMenu';
+export { Counter } from './counter';
+export { default as MediaCard } from './productCard';
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { Navbar } from './layout/Navbar/Navbar';
+export * from './pages';
