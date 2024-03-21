2b2b579b9a7836a2c2980bf716f8cd851d4898dc
SofiaBellomusto <sofiabellomusto2001@gmail.com>
2024-03-21 20:01:50 -0300

    cart widget  update

--- src/components/common/CartWidget.jsx
+++ src/components/common/CartWidget.jsx
@@ -1,15 +1,20 @@
 import { Badge } from "@mui/material";
 import { Link } from "react-router-dom";
 import { BsFillCartCheckFill } from "react-icons/bs";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { useContext } from 'react';
+import { CartContext } from '../../Context/CartContext';
 
 export const CartWidget = () => {
-    return (
-        <Link to="/cart">
-            <Badge badgeContent={0} showZero color="primary">
-                <BsFillCartCheckFill size="30px" color="beige" />
-            </Badge>
-        </Link>
-    );
+  const { cart } = useContext(CartContext); 
+
+  const totalItems = cart.reduce((total, producto) => total + producto.quantity, 0);
+
+  return (
+    <Link to="/cart">
+      <Badge badgeContent={totalItems} showZero color="primary">
+        <BsFillCartCheckFill size="30px" color="beige" />
+      </Badge>
+    </Link>
+  );
 }
 
